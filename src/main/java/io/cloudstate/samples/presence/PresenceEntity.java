eb51fc65e02a75cea30a94d7bdfdec4d5b0f74af
Justin Heyes-Jones <justin.heyes-jones@yoppworks.com>
2020-06-06 09:14:58 -0700

    implement monitor

--- src/main/java/io/cloudstate/samples/presence/PresenceEntity.java
+++ src/main/java/io/cloudstate/samples/presence/PresenceEntity.java
@@ -1,12 +1,7 @@
 package io.cloudstate.samples.presence;
 
-// import java.util.Arrays;
-// import java.util.Iterator;
 import java.util.Optional;
 import java.util.function.Consumer;
-
-// import akka.NotUsed;
-// import akka.stream.javadsl.Source;
 import io.cloudstate.javasupport.crdt.*;
 import cloudstate.samples.chat.presence.grpc.*;
 
@@ -20,23 +15,7 @@ public class PresenceEntity {
   public PresenceEntity(Vote presence) {
     this.presence = presence;
     System.out.println("Created entity");
-
   }
-
-
-  /**
-   * Connect a user, to make their presence active.
-   *
-   * This is a streamed call. As long as a user (id given by the entity id) is connected
-   * to it, they are considered to be online.
-   *
-   * Here we use a Vote CRDT, which if at least one node votes is true, will be true.
-   * So when the user connects, we invoke the connect() method (which we have defined
-   * by enriching the CRDT in onStateSet), which will manage our vote accordingly.
-   *
-   * When they disconnect, the onStreamCancel callback is invoked, and we disconnect,
-   * removing our vote if this is the last connection to this CRDT.
-   */
 
   private Optional<Vote>changeHandler(SubscriptionContext subcription) {
     System.out.println("change!");
@@ -53,6 +32,41 @@ public class PresenceEntity {
     System.out.println("cancel! users " + users);
   };
 
+  /**
+   * User presence monitoring call.
+   *
+   * This is a streamed call. We add a onStateChange callback, so that whenever the CRDT
+   * changes, if the online status has changed since the last message we pushed, we push
+   * it.
+   */
+  @CommandHandler OnlineStatus monitor(User user, StreamedCommandContext<Vote> ctx) {
+    Boolean onlineStatus = presence.isAtLeastOne();
+
+    if(ctx.isStreamed()) {
+      ctx.onChange(subCtx ->
+       {
+          System.out.println("monitor: " + user.getName() + " return {" + onlineStatus + "}");
+          return Optional.of(presence);
+       });
+    }
+
+    System.out.println("monitor: " + user.getName() + " return {" + onlineStatus + "}");
+    return OnlineStatus.newBuilder().setOnline(onlineStatus).build();
+  }
+
+   /**
+   * Connect a user, to make their presence active.
+   *
+   * This is a streamed call. As long as a user (id given by the entity id) is connected
+   * to it, they are considered to be online.
+   *
+   * Here we use a Vote CRDT, which if at least one node votes is true, will be true.
+   * So when the user connects, we invoke the connect() method (which we have defined
+   * by enriching the CRDT in onStateSet), which will manage our vote accordingly.
+   *
+   * When they disconnect, the onStreamCancel callback is invoked, and we disconnect,
+   * removing our vote if this is the last connection to this CRDT.
+   */
   @CommandHandler
   public Empty connect(StreamedCommandContext<Vote> ctx) {
 
